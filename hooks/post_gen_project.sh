4c0c36e70308e2275290c62638879b70633746da
André Mathlener <andre.mathlener@ns.nl>
2025-01-23 14:33:43 +0100

    Fix shell script

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,12 +3,19 @@
 git init
 {{cookiecutter.build_tool}} install
 
+create_github_repo="{{ cookiecutter.create_github_repo }}"
+
 git add -A
 git commit -m "Initial commit"
-{%- if cookiecutter.create_github_repo -%}
-    gh repo create --public {{cookiecutter.__project_slug}}
-{% endif %}
 git remote add origin {{cookiecutter.__git_repo}}
+
+if [ "$create_github_repo" = "True" ]; then
+    echo ""
+    echo "Creating and pushing GitHub repository..."
+    gh repo create {{cookiecutter.__project_slug}} --public
+    git push origin main
+fi
+
 echo ""
 echo "Project initialized. Happy coding!"
 echo "To open IntelliJ IDEA, run 'idea {{cookiecutter.__project_slug}}'"

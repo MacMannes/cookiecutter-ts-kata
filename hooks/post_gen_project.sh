0c6d23ebfd1352bc7c8e778606e3397fe60887bf
André Mathlener <andre.mathlener@ns.nl>
2025-01-23 15:40:58 +0100

    use sed to replace `clazz` with a better looking instance name

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,6 +4,23 @@ git init
 {{cookiecutter.build_tool}} install
 
 create_github_repo="{{ cookiecutter.create_github_repo }}"
+class_name="{{ cookiecutter.__class_name }}"
+instance_name="$(echo "${class_name:0:1}" | tr '[:upper:]' '[:lower:]')${class_name:1}"
+
+typescript_file="src/{{cookiecutter.__project_slug }}.test.ts"
+
+# Use sed to replace "clazz" with `instance_name` in the TypeScript file
+if [ -f "$typescript_file" ]; then
+    # For GNU sed
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sed -i "s/clazz/$instance_name/g" "$typescript_file"
+    # For BSD sed (e.g., macOS)
+    else
+        sed -i '' "s/clazz/$instance_name/g" "$typescript_file"
+    fi
+else
+    echo "TypeScript file not found: $typescript_file"
+fi
 
 git add -A
 git commit -m "Initial commit"

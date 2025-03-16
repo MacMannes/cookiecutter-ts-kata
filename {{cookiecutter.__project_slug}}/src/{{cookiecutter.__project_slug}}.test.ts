484007242d6bd9642c38e599d53060700a338102
André Mathlener <andre.mathlener@ns.nl>
2025-03-16 15:43:15 +0100

    fix: import error

--- {{cookiecutter.__project_slug}}/src/{{cookiecutter.__project_slug}}.test.ts
+++ {{cookiecutter.__project_slug}}/src/{{cookiecutter.__project_slug}}.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { {{cookiecutter.__class_name}} } from 'src/{{cookiecutter.__project_slug}}';
+import { {{cookiecutter.__class_name}} } from 'src/{{cookiecutter.__project_slug}}.ts';
 
 describe('{{cookiecutter.project_name}}', () => {
     it('Creating an instance of the {{cookiecutter.__class_name}} class should work', () => {
